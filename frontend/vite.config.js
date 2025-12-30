65bd5d78247ef36011e0efecbddc74c4739a9f2c
indkshitij <ind.kshitijsingh@gmail.com>
2025-12-31 00:04:02 +0530

    fix: config file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base:process.env.VITE_BACKEND_URL || "https://task-manager-wj6n.vercel.app/api"
 });
